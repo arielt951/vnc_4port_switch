6dec4ab4fb4ea35d5ac50b9800090052a8748694
Yuval Horowitz <yh3@micron-x01.eng.tau.ac.il>
2025-12-07 10:39:55 +0200

    asd

--- csrc/filelist.cu
+++ csrc/filelist.cu
@@ -4,16 +4,16 @@ ifeq ($(LDVERSION), 0)
 endif
 
 ARCHIVE_OBJS=
-ARCHIVE_OBJS += _783003_archive_1.so
-_783003_archive_1.so : archive.82/_783003_archive_1.a
+ARCHIVE_OBJS += _120511_archive_1.so
+_120511_archive_1.so : archive.89/_120511_archive_1.a
 	@$(AR) -s $<
-	@$(PIC_LD) -shared  -Bsymbolic $(LD_NORELAX_FLAG)  -o .//../simv.daidir//_783003_archive_1.so --whole-archive $< --no-whole-archive
+	@$(PIC_LD) -shared  -Bsymbolic $(LD_NORELAX_FLAG)  -o .//../simv.daidir//_120511_archive_1.so --whole-archive $< --no-whole-archive
 	@rm -f $@
-	@ln -sf .//../simv.daidir//_783003_archive_1.so $@
+	@ln -sf .//../simv.daidir//_120511_archive_1.so $@
 
 
 ARCHIVE_OBJS += _prev_archive_1.so
-_prev_archive_1.so : archive.82/_prev_archive_1.a
+_prev_archive_1.so : archive.89/_prev_archive_1.a
 	@$(AR) -s $<
 	@$(PIC_LD) -shared  -Bsymbolic $(LD_NORELAX_FLAG)  -o .//../simv.daidir//_prev_archive_1.so --whole-archive $< --no-whole-archive
 	@rm -f $@
