b11abde0a4db2fe8cbc2358cd06d89abae716c8b
ariel <arielt951@gmail.com>
2026-01-01 13:53:59 +0200

    fixes

--- Synt/run_gls.sh
+++ Synt/run_gls.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-# 1. Define Paths
-SAED32="/data/synopsys/lib/saed32nm/lib/std/verilog/saed32nm.v"
+# 1. Define Paths (Found via your search)
+# We include HVT, RVT, and LVT to ensure all cell types are covered.
+HVT="/data/synopsys/lib/SAED32_EDK/lib/stdcell_hvt/verilog/saed32nm_hvt.v"
+RVT="/data/synopsys/lib/SAED32_EDK/lib/stdcell_rvt/verilog/saed32nm.v"
+LVT="/data/synopsys/lib/SAED32_EDK/lib/stdcell_lvt/verilog/saed32nm_lvt.v"
+
 NETLIST="./switch_4port_netlist.v"
 
 # 2. Compile
-# Added -suppress=IPD to ignore netlist naming conflicts
+# Notice we now use multiple -v flags to include all library files
 vcs -sverilog -debug_access+all -full64 -kdb \
     -timescale=1ns/1ps \
     -top switch_test \
-    -v $SAED32 \
+    -v $HVT \
+    -v $RVT \
+    -v $LVT \
     +define+SDF_ANNOTATE \
-    -suppress=IPD \
     ../design/packet_pkg.sv \
     ../design/port_if.sv \
     ../verification/assertions.sv \
@@ -31,5 +36,5 @@ if [ $? -eq 0 ]; then
     echo "GLS Compilation Successful. Launching Verdi..."
     ./simv_gls -gui=verdi
 else
-    echo "GLS Compilation Failed! (Check syntax errors above)"
+    echo "GLS Compilation Failed!"
 fi
